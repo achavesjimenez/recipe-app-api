9ba928cdec3411204a833e9e1ee0074dc41f09e1
Alberto Chaves <alberto.chaves@travelperk.com>
2020-07-30 14:38:23 +0200

    Tags endpoint and model added

---
+++ app/recipe/views.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Tag
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage tags in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """return tags from current authenticated user only
+
+            when the view is invoked, this method is used to retrieve the tags,
+            if this method is not override, whatever is specified in queryset
+            field
+        """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
