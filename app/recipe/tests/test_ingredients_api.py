fd2337f58e443f0266bf5d3d606597de82062fff
Alberto Chaves <alberto.chaves@travelperk.com>
2020-07-30 17:23:36 +0200

    ingredient rest resource added

---
+++ app/recipe/tests/test_ingredients_api.py
@@ -0,0 +1,68 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+
+class PublicIngredientsApiTests(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """test that login is required to access ingredient resource"""
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsApiTests(TestCase):
+    """Test for private ingredients methods"""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'u@gmail.com',
+            '123456'
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredient_list(self):
+        """test retrieving the list of ingredients"""
+        Ingredient.objects.create(user=self.user, name="Beef")
+        Ingredient.objects.create(user=self.user, name="Cod")
+
+        res = self.client.get(INGREDIENTS_URL)
+
+        # ordered by name in reverse order
+        all_ingredients = Ingredient.objects.all().order_by('-name')
+        expected_data = IngredientSerializer(all_ingredients, many=True).data
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 2)
+        self.assertEqual(res.data, expected_data)
+
+    def test_only_authenticated_user_ingredients_retrieved(self):
+        """test that only ingredients from the authenticated user are
+           retrieved
+        """
+        user2 = get_user_model().objects.create_user(
+            'u2@gmail.com',
+            '123456'
+        )
+
+        ingredient = Ingredient.objects.create(user=self.user, name='Apple')
+        Ingredient.objects.create(user=user2, name='Guinness')
+        res = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0], IngredientSerializer(ingredient).data)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 from recipe import serializers
 
 
@@ -26,3 +26,20 @@ class TagViewSet(viewsets.GenericViewSet,
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage ingredients in DB"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """return tags from current authenticated user only
+
+            when the view is invoked, this method is used to retrieve
+             ingredients, if this method is not override, whatever is
+             specified in queryset field
+        """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
